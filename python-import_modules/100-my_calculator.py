917852b283185a40505ed5b842241322aba09f98
YusifAlizadeh <alizade.yusif111@gmail.com>
2025-11-17 09:06:56 +0000

    test

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import sys
 from calculator_1 import add, sub, mul, div
+import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -23,4 +23,4 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print("{} {} {} = {}".format(a, operator, b, result))
+    print("{} {} {} = {}".format(a, operator, b, result)
