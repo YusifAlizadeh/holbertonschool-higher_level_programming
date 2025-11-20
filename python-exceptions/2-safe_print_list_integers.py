359935813cb34de8b9710e612090571f5d44d338
YusifAlizadeh <alizade.yusif111@gmail.com>
2025-11-20 10:51:03 +0000

    fefef

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,3 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
             pass
     print()
     return count
+~                                                                                                                                                                                                                                                                             
+~                                
