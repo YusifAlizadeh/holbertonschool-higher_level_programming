bbc8ccff8c93a4f99bc09f191f1989de37f5cf7a
YusifAlizadeh <alizade.yusif111@gmail.com>
2025-11-27 08:23:43 +0000

    yesy

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+BaseGeometry based game in python
+'''
+
+class BaseGeometry:
+    '''
+    BaseGeometry
+    '''
+    def area(self):
+        '''
+        Lessssonssnsn
+        '''
+        raise Exception("area() is not implemented")
