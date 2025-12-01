e23c366cffb8c6488c6a65d3514b6d3bf52112cc
YusifAlizadeh <alizade.yusif111@gmail.com>
2025-12-01 07:01:46 +0000

    fetching

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""
+Fetches https://intranet.hbtn.io/status using urllib.
+
+Requirements:
+- Use urllib only
+- Use a with statement
+- Display the body in a specific format
+"""
+
 import urllib.request
-'''
-Yes it is
-'''
 
 
-if __name__ == "__main__":
+def main():
+    """Fetch the URL and display the formatted response."""
     url = "https://intranet.hbtn.io/status"
 
     with urllib.request.urlopen(url) as response:
@@ -14,3 +21,7 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    main()
