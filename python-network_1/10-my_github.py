71e5d4286003fedaff8a97073844d2dc024fd8ed
YusifAlizadeh <alizade.yusif111@gmail.com>
2025-12-01 07:55:48 +0000

    Add script that fetches GitHub user ID using Basic Authentication

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Displays the GitHub user id using Basic Authentication with a personal access token.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    response = requests.get(url, auth=(username, token))
+
+    if response.status_code == 200:
+        print(response.json().get("id"))
+    else:
+        print(None)
